f90495422de0929f24272528d7516ef11ce3c439
Paul Savignano <Paul.Savignano@gmail.com>
2017-01-02 13:04:10 -0800

    Add RootQuery and RootMutation APIs.

--- server.js
+++ server.js
@@ -1,11 +1,53 @@
-import expresss from 'express';
+import express from 'express';
 
 const app = express();
 
-app.use('/graphql', (req, res) => {
-  res.send({ data: true });
+import graphqlHTTP from 'express-graphql';
+import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLNonNull, GraphQLID } from 'graphql';
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQuery',
+  description: 'The root query',
+  fields: {
+    viewer: {
+      type: GraphQLString,
+      resolve() {
+        return 'viewer!';
+      }
+    }
+  }
 });
 
+const Schema = new GraphQLSchema({
+  query: RootQuery
+});
+
+app.use('/graphql', graphqlHTTP({ schema: Schema, graphiql: true }));
+
 app.listen(3000, () => {
-  console.log({ running: true })
-})
+  console.log({ running: true });
+  console.log('The password is: mypassword1');
+});
+
+let inMemoryStore = {};
+const RootMutation = new GraphQLObjectType({
+  name: 'RootMutation',
+  description: 'The root mutation',
+  fields: {
+    setNode: {
+      type: GraphQLString,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID),
+        },
+        value: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve(source, args) {
+        inMemoryStore[args.key] = args.value;
+        return inMemoryStore[args.key];
+      }
+    }
+  },
+});
